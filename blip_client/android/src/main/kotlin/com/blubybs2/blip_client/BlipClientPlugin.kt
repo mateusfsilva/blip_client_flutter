27553d8b088fa57bf99f76d841d5dd92fb268058
Mateus Gustavo de Freitas e Silva <mateusfsilva@gmail.com>
2022-03-28 15:50:17 -0300

    Bump blip-client version

--- blip_client/android/src/main/kotlin/com/blubybs2/blip_client/BlipClientPlugin.kt
+++ blip_client/android/src/main/kotlin/com/blubybs2/blip_client/BlipClientPlugin.kt
@@ -6,7 +6,6 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 import net.take.blipchat.AuthType
 import net.take.blipchat.BlipClient
 import net.take.blipchat.models.Account
