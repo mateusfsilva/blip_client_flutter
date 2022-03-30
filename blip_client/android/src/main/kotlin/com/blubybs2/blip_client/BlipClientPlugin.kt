3b448c190b45dc83e515e449e261b4da57ca5d4e
Mateus Gustavo de Freitas e Silva <mateusfsilva@gmail.com>
2022-03-30 18:23:53 -0300

    Update maven repository

--- blip_client/android/src/main/kotlin/com/blubybs2/blip_client/BlipClientPlugin.kt
+++ blip_client/android/src/main/kotlin/com/blubybs2/blip_client/BlipClientPlugin.kt
@@ -6,6 +6,7 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
 import net.take.blipchat.AuthType
 import net.take.blipchat.BlipClient
 import net.take.blipchat.models.Account
